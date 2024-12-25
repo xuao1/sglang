033341b25473ba94c2d4b8c19dc48b4160692f73
xuao1 <xuao123@mail.ustc.edu.cn>
2024-12-25 16:16:11 +0800

    update

--- python/sglang/sheet2_2.sh
+++ python/sglang/sheet2_2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # 设置模型路径
-MODEL_PATH="/workspace/model/Mistral-7B-Instruct-v0.1/"
+MODEL_PATH="/model/Meta-Llama-3-8B-Instruct/"
 
 # 定义批处理大小和输入长度数组
 # batch_sizes=(1 2 4 8 16)
 # input_lengths=(128 256 384 512 640 768 896 1024 1152 1280 1408 1536 1664 1792 1920 2048)
-batch_sizes=(16 8 4 4 1)
+batch_sizes=(16 8 4 2 1)
 input_lengths=(2048 1920 1792 1664 1536 1408 1280 1152 1024 896 768 640 512 384 256 128)
 
 # 设定一个足够大的输出长度
@@ -22,8 +22,7 @@ for batch in "${batch_sizes[@]}"; do
         echo "Testing with batch_size=$batch and input_len=$input_len" >> "$output_file"
 
         # 运行推理命令，只将标准输出重定向到文件中
-        python -m sglang.bench_one_batch --model-path "$MODEL_PATH" --batch "$batch" --input-len "$input_len" --output-len "$output_length" >> "$output_file" 
-        
+        python -m sglang.bench_one_batch --model-path "$MODEL_PATH" --batch "$batch" --input-len "$input_len" --output-len "$output_length" >> "$output_file"         
         # 检查是否有错误发生
         if [ $? -ne 0 ]; then
             echo "Error occurred with batch_size=$batch and input_len=$input_len" >> "$output_file"
